a61ed98e0afc80d6b0c09ff0b1cd73c662c83bf6
Christopher Robinson <crobinson3223@outlook.com>
2023-02-08 11:48:25 -0800

    Added files

---
+++ ImageResizeWebApp/Program.cs
@@ -0,0 +1,43 @@
+using ImageResizeWebApp.Models;
+using ImageResizeWebApp.Services;
+
+namespace ImageResizeWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            // Add Storage Interop/Helper to the project
+            var storageConfig = builder.Configuration.GetSection("AzureStorageConfig");
+            builder.Services.AddScoped<IStorageInterop, StorageHelper>().Configure<AzureStorageConfig>(storageConfig);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
